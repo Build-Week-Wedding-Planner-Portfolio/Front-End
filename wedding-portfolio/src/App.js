1dd165b21cd9fa7bd8d5972250213fef64016ec2
Farid Hamida <faridhamida22@gmail.com>
2019-11-20 08:47:13 -0800

    Updates

--- wedding-portfolio/src/App.js
+++ wedding-portfolio/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import './styles.css';
 import HomePage from "./components/HomePage";
 import LoginForm from "./components/Login";
-import SignUp from "./components/SignUp";
 import PlannerCreatePost from "./components/plannerComponents/PlannerCreatePost";
 import PlannerEditForm from "./components/plannerComponents/PlannerEditForm";
 import PlannerPortfolio from "./components/plannerComponents/PlannerPortfolio";
 import GuestSearchForm from "./components/guestComponents/GuestSearchForm";
 import NavBar from "./components/NavBar";
 import PrivateRoute from "./utils/PrivateRoute";
-import {Route, Link} from "react-router-dom"
+import {Route, Link} from "react-router-dom";
+import SignUp from "./components/SignUp";
 
 
 
@@ -21,7 +21,7 @@ function App() {
       <NavBar />
       <Route exact path="/homepage" component={HomePage} />
       <Route path='/guest' component ={GuestSearchForm} />
-
+      <Route path='/signup' component={SignUp} />
      <PrivateRoute  path='/plannerportfolio' component={PlannerPortfolio}/>
      <Route path='/testplannerportfolio/:id' component={PlannerPortfolio} />
      <PrivateRoute path='/plannerportfolio' component={PlannerPortfolio}/>
