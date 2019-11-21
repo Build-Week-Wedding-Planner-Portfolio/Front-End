b959aa9d50f63209bbc40167a27d4c6d2ddd73e2
sumeyaali <thesumeyaali@gmail.com>
2019-11-21 10:44:54 -0800

    redid loginForm using formik

--- wedding-portfolio/src/components/Login.js
+++ wedding-portfolio/src/components/Login.js
@@ -1,36 +1,66 @@
-import React from "react";
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, {useState, useEffect} from "react";
+// import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+// import useForm from "react-hook-form";
 
 
-export default function LoginForm () {
+
+const LoginForm = ({ values, errors, touched, status }) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+      status && setUsers(users => [...users, status]);
+    }, [status]);
+
     return (
-        <div className="body"> 
-        <image src = "" />
-        <Form className="login">
-            <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input 
-                type="email" 
-                name="email" 
-                id="exampleEmail" 
-                placeholder="Email" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="examplePassword">Password</Label>
-                <Input 
-                type="password" 
-                name="password" 
-                id="examplePassword" 
-                placeholder="Password (6 or more characters" />
-            </FormGroup>
-            <Button className="loginButton">Login</Button>
-        </Form>
+        <div>
+          <Form>
+            <Field type="email" name="email" placeholder="Email" />
+            {touched.email && errors.email && (
+              <p className="errors">{errors.email}</p>
+            )}
+    
+            <Field type="password" name="password" placeholder="Password" />
+            {touched.password && errors.password && <p className="errors">{errors.password}</p>}
+    
+            <button className="loginButton" type="submit">Submit</button>
+          </Form>
+          {users.map(user => (
+            <ul key={user.id}>
+              <li> email: {user.email}</li>
+              <li>password: {user.password}</li>
+            </ul>
+          ))}
         </div>
-    )
+      );
+    };
+    const FormikLoginForm = withFormik({
+        mapPropsToValues({ email, password }) {
+          return {
+            email: email || "",
+            password: password || "",
+          };
+        },
+        validationSchema: Yup.object().shape({
+            email: Yup.string().required(),
+            password: Yup.string().required()
+          }),
+          handleSubmit(values, { setStatus }) {
 
-}
+            axios
+            .post("https://weddingplannerlambda.herokuapp.com/api/auth/login")
+            .then(res => {
+              setStatus(res.data);
+              console.log(res);
+            })
+            .catch(err => console.log(err.response));
+        }
+      })(LoginForm);
 
 
+
+export default FormikLoginForm;
 
 
 
